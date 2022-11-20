a1f205689f4094534ce0f0105f9c193f792cc666
Luis González <luis.gonzalez.ramirez@estudiantat.upc.edu>
2022-11-20 16:17:05 +0100

    better resource management

--- Game/Source/resource.h
+++ Game/Source/resource.h
@@ -1,5 +1,5 @@
 //{{NO_DEPENDENCIES}}
-// Archivo de inclusi¨®n generado de Microsoft Visual C++.
+// Archivo de inclusión generado de Microsoft Visual C++.
 // Usado por ANormalWalkInElPueblo.rc
 //
 #define IDI_ICON1                       101
@@ -10,7 +10,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
